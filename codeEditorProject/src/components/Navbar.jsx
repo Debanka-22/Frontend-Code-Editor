4d4c6b8cae504b250a5c3da58c784dadbcc12b8f
Debanka_22 <samantadebanka@gmail.com>
2025-06-25 01:12:32 +0530

    first commit

---
+++ codeEditorProject/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div
+            className='main lg:flex md:flex flex-wrap justify-between items-center px-4 bg-[#2f3640] py-4'>
+        </div>
+    )
+}
+
+export default Navbar
